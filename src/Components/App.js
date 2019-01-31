39c945a4ba98ff4c5dca1d2810804e71cc2e8394
Rodrigo Vieira <rodriigovieira@gmail.com>
2019-01-31 10:52:40 -0300

    adding pagination

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,24 +9,34 @@ export default class App extends Component {
   state = {
     movieData: [],
     genreList: [],
+    displayData: [],
     displayLoading: false,
     ready: false,
-    instructions: true
+    instructions: true,
+    resultsPerPage: 5,
+    currentPage: 1,
   };
 
   handleSubmit = searchText => {
     this.setState({ displayLoading: true, ready: false, instructions: false });
+
+    const { resultsPerPage, currentPage } = this.state;
+    const indexOfLastResult = resultsPerPage * currentPage;
+    const indexOfFirstResult = indexOfLastResult - resultsPerPage;
 
     const apiLink = `https://api.themoviedb.org/3/search/multi?api_key=3a9b881a75eeb15cfc1a9051e9889d7f&language=pt-BR&query=${searchText}&page=1&include_adult=true`;
 
     fetch(apiLink)
       .then(response => response.json())
       .then(movieData => {
+        const displayData = movieData.results.slice(indexOfFirstResult,indexOfLastResult);
+
         this.setState({
-          movieData,
+          movieData: movieData.results,
+          displayData,
           displayLoading: false,
           ready: true,
-          instructions: false,
+          instructions: false
         });
       });
   };
@@ -40,6 +50,63 @@ export default class App extends Component {
 
   emptyList = () => {
     this.setState({ ready: false, instructions: true });
+  };
+
+  renderResults = () => {
+    return this.state.displayData.map((movie, index) => {
+      let genreList = [];
+
+      if (movie.genre_ids) {
+        genreList = this.state.genreList
+          .filter(genre => movie.genre_ids.includes(genre.id))
+          .map(value => value.name);
+      }
+
+      return (
+        <Movie
+          key={index}
+          title={movie.title}
+          rating={movie.vote_average}
+          date={movie.release_date}
+          overview={movie.overview}
+          image={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${movie.poster_path}`}
+          genres={genreList}
+        />
+      );
+    });
+  };
+
+  handlePageChange = (event) => {
+    const indexOfLastResult = this.state.resultsPerPage * event.target.id;
+    const indexOfFirstResult = indexOfLastResult - this.state.resultsPerPage;
+    const displayData = this.state.movieData.slice(indexOfFirstResult, indexOfLastResult);
+
+    this.setState({
+      currentPage: event.target.id,
+      displayData,
+    })
+  }
+
+  renderPageList = () => {
+    const { movieData, resultsPerPage } = this.state;
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(movieData.length / resultsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    return pageNumbers.map((number, index) => {
+      let pageStyle = number === this.state.currentPage ? 'page-link-selected' : 'page-link';
+      return (
+        <button
+          className={pageStyle}
+          key={index}
+          onClick={this.handlePageChange}
+          id={number}
+        >
+          {number}
+        </button>
+      )
+    })
   }
 
   render() {
@@ -54,33 +121,22 @@ export default class App extends Component {
         )}
 
         {/* Show instructions message */}
-        {this.state.instructions &&
-          <p>Digite acima o nome do filme ou o gênero que você gostaria de buscar.</p>
-        }
+        {this.state.instructions && (
+          <p>Digite acima o nome do filme ou o gênero que você gostaria debuscar.</p>
+        )}
 
         {/* Render Search Results */}
-        {this.state.ready &&
-          this.state.movieData.results.map(movie => {
-              let genreList = [];
-              if (movie.genre_ids) {
-              genreList = this.state.genreList
-                .filter(genre => movie.genre_ids.includes(genre.id))
-                .map(value => value.name);
-              }
+        {this.state.ready && this.renderResults()}
 
-            return (
-              <Movie
-                key={Math.random()}
-                title={movie.title}
-                rating={movie.vote_average}
-                date={movie.release_date}
-                overview={movie.overview}
-                image={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${movie.poster_path}`}
-                genres={genreList}
-              />
-            );
-          })}
-          {this.state.ready && <button onClick={this.emptyList} >Esvaziar Lista</button>}
+        {/* Show Pagination */}
+        {this.state.ready && this.renderPageList()}
+
+        <br />
+
+        {/* Show clear screen button */}
+        {this.state.ready && (
+          <button onClick={this.emptyList}>Esvaziar Lista</button>
+        )}
       </div>
     );
   }
